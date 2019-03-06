bc219981fb50464834752306092e13d682c7b0e9
gsr1001 <44772106+gsr1001@users.noreply.github.com>
2019-03-06 09:56:48 +0100

    Dimensiones de la imagen
    img_tam_x y img_tam_y actualizado

--- src/Ball.java
+++ src/Ball.java
@@ -15,8 +15,8 @@ public class Ball {
 		image = ii.getImage();
 		
 		//TODO Depend of image size
-		IMG_TAM_X = 32;
-		IMG_TAM_Y = 32;
+		IMG_TAM_X = 728;
+		IMG_TAM_Y = 728;
 
 		
 		x = Billiards.Width/4-16;
